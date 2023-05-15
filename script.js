66c10b286d9875d9d2bc59429b966e21edcaaa40
mayreli <mayrelioregon@gmail.com>
2023-05-15 00:52:27 -0600

    se corrige la funcion copiar

--- script.js
+++ script.js
@@ -56,7 +56,31 @@ function desencriptar (stringDesencriptada){
 }
 
 function copiar(){
-  navigator.clipboard.writeText(textAreaDesencriptado.value);
+   fallbackCopyTextToClipboard(textAreaDesencriptado.value);
+
   alert("Texto Copiado");
 }
+
+function fallbackCopyTextToClipboard(text) {
+    const textArea = document.createElement("textarea");
+    textArea.value = text;
+  
+    // Avoid scrolling to bottom
+    textArea.style.top = "0";
+    textArea.style.left = "0";
+    textArea.style.position = "fixed";
+  
+    document.body.appendChild(textArea);
+    textArea.focus();
+    textArea.select();
+  
+    try {
+      const successful = document.execCommand("copy");
+      const msg = successful ? "successful" : "unsuccessful";
+    } catch (err) {
+      console.error("Error al copiar", err);
+    }
+  
+    document.body.removeChild(textArea);
+  }
 
